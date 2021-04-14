f046f6661ccf97e1622208ab3668c90db58a56b6
lizhongde@yuchen.net.cn <>
2021-04-14 15:12:20 +0800

    初始化

---
+++ src/main/java/com/yuchen/data/OriginApplication.java
@@ -0,0 +1,20 @@
+package com.yuchen.data;
+
+import com.yuchen.data.config.DataSourcePrimary;
+import com.yuchen.data.config.DataSourceSecond;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import javax.xml.crypto.Data;
+
+@SpringBootApplication
+@EnableConfigurationProperties(value = { DataSourcePrimary.class, DataSourceSecond.class })
+//@MapperScan("com.yuchen.data.mapper.*")
+public class OriginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OriginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuchen/data/OriginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuchen.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
